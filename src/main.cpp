089b7f9e124914e32188f77cd777af8dfc3d1f7f
zhanglin-wu <zhang-lin.wu@perkinelerm.com>
2018-12-24 19:28:22 +0800

    Safe clean up on the coding style issues

--- src/main.cpp
+++ src/main.cpp
@@ -15,141 +15,161 @@ using namespace std;
 // For convenience
 using json = nlohmann::json;
 
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
-
-// Checks if the SocketIO event has JSON data.
-// If there is data the JSON object in string format will be returned,
-// else the empty string "" will be returned.
-string hasData(string s)
+namespace
 {
-    auto found_null = s.find("null");
-    auto b1 = s.find_first_of("[");
-    auto b2 = s.find_first_of("}");
-    if (found_null != string::npos)
+    constexpr const double kMaxSpeed = 49.5;
+    constexpr const double kMaxAcc = 0.224;
+    
+    // The max s value before wrapping around the track back to 0
+    constexpr const double kMaxS = 6945.554;
+    
+    // For converting back and forth between radians and degrees.
+    constexpr double pi()
     {
+        return M_PI;
+    }
+    
+    double deg2rad(double x)
+    {
+        return x * pi() / 180;
+    }
+    
+    double rad2deg(double x)
+    {
+        return x * 180 / pi();
+    }
+
+    // Checks if the SocketIO event has JSON data.
+    // If there is data the JSON object in string format will be returned,
+    // else the empty string "" will be returned.
+    string hasData(string s)
+    {
+        auto found_null = s.find("null");
+        auto b1 = s.find_first_of("[");
+        auto b2 = s.find_first_of("}");
+        if (found_null != string::npos)
+        {
+            return "";
+        }
+        else if (b1 != string::npos && b2 != string::npos)
+        {
+            return s.substr(b1, b2 - b1 + 2);
+        }
+        
         return "";
     }
-    else if (b1 != string::npos && b2 != string::npos)
+
+    double distance(double x1, double y1, double x2, double y2)
     {
-        return s.substr(b1, b2 - b1 + 2);
+        return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
     }
-    
-    return "";
-}
 
-double distance(double x1, double y1, double x2, double y2)
-{
-    return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-}
-
-int ClosestWaypoint(double x, double y, const vector<double>& maps_x, const vector<double>& maps_y)
-{
-    double closestLen = 100000; // Large number
-    int closestWaypoint = 0;
-    
-    for(int i = 0; i < maps_x.size(); i++)
+    int ClosestWaypoint(double x, double y, const vector<double>& maps_x, const vector<double>& maps_y)
     {
-        double map_x = maps_x[i];
-        double map_y = maps_y[i];
-        double dist = distance(x, y, map_x, map_y);
-        if(dist < closestLen)
+        double closestLen = 100000; // Large number
+        int closestWaypoint = 0;
+        
+        for(int i = 0; i < maps_x.size(); i++)
         {
-            closestLen = dist;
-            closestWaypoint = i;
+            double map_x = maps_x[i];
+            double map_y = maps_y[i];
+            double dist = distance(x, y, map_x, map_y);
+            if(dist < closestLen)
+            {
+                closestLen = dist;
+                closestWaypoint = i;
+            }
         }
+        
+        return closestWaypoint;
     }
-    
-    return closestWaypoint;
-}
 
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-    int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
-    
-    double map_x = maps_x[closestWaypoint];
-    double map_y = maps_y[closestWaypoint];
-    
-    double heading = atan2((map_y-y), (map_x-x));
-    double angle = abs(theta - heading);
-    if(angle > pi()/4)
+    int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
     {
-        closestWaypoint++;
+        int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
+        
+        double map_x = maps_x[closestWaypoint];
+        double map_y = maps_y[closestWaypoint];
+        
+        double heading = atan2((map_y-y), (map_x-x));
+        double angle = abs(theta - heading);
+        if(angle > pi()/4)
+        {
+            closestWaypoint++;
+        }
+        
+        return closestWaypoint;
     }
-    
-    return closestWaypoint;
-}
 
-// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-    int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
-    
-    int prev_wp;
-    prev_wp = next_wp-1;
-    if(next_wp == 0)
+    // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+    vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
     {
-        prev_wp  = maps_x.size()-1;
+        int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+        
+        int prev_wp;
+        prev_wp = next_wp-1;
+        if(next_wp == 0)
+        {
+            prev_wp  = maps_x.size()-1;
+        }
+        
+        double n_x = maps_x[next_wp]-maps_x[prev_wp];
+        double n_y = maps_y[next_wp]-maps_y[prev_wp];
+        double x_x = x - maps_x[prev_wp];
+        double x_y = y - maps_y[prev_wp];
+        
+        // Find the projection of x onto n
+        double proj_norm = (x_x*n_x + x_y*n_y) / (n_x*n_x + n_y*n_y);
+        double proj_x = proj_norm * n_x;
+        double proj_y = proj_norm * n_y;
+        
+        double frenet_d = distance(x_x, x_y, proj_x, proj_y);
+        
+        // See if d value is positive or negative by comparing it to a center point
+        double center_x = 1000-maps_x[prev_wp];
+        double center_y = 2000-maps_y[prev_wp];
+        double centerToPos = distance(center_x,center_y,x_x,x_y);
+        double centerToRef = distance(center_x,center_y,proj_x,proj_y);
+        if(centerToPos <= centerToRef)
+        {
+            frenet_d *= -1;
+        }
+        
+        // Calculate s value
+        double frenet_s = 0;
+        for(int i = 0; i < prev_wp; i++)
+        {
+            frenet_s += distance(maps_x[i], maps_y[i], maps_x[i + 1], maps_y[i + 1]);
+        }
+        
+        frenet_s += distance(0, 0, proj_x, proj_y);
+        return {frenet_s, frenet_d};
     }
-    
-    double n_x = maps_x[next_wp]-maps_x[prev_wp];
-    double n_y = maps_y[next_wp]-maps_y[prev_wp];
-    double x_x = x - maps_x[prev_wp];
-    double x_y = y - maps_y[prev_wp];
-    
-    // Find the projection of x onto n
-    double proj_norm = (x_x*n_x + x_y*n_y) / (n_x*n_x + n_y*n_y);
-    double proj_x = proj_norm * n_x;
-    double proj_y = proj_norm * n_y;
-    
-    double frenet_d = distance(x_x, x_y, proj_x, proj_y);
-    
-    // See if d value is positive or negative by comparing it to a center point
-    double center_x = 1000-maps_x[prev_wp];
-    double center_y = 2000-maps_y[prev_wp];
-    double centerToPos = distance(center_x,center_y,x_x,x_y);
-    double centerToRef = distance(center_x,center_y,proj_x,proj_y);
-    if(centerToPos <= centerToRef)
-    {
-        frenet_d *= -1;
-    }
-    
-    // Calculate s value
-    double frenet_s = 0;
-    for(int i = 0; i < prev_wp; i++)
-    {
-        frenet_s += distance(maps_x[i], maps_y[i], maps_x[i + 1], maps_y[i + 1]);
-    }
-    
-    frenet_s += distance(0, 0, proj_x, proj_y);
-    return {frenet_s, frenet_d};
-}
 
-// Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-    int prev_wp = -1;
-    
-    while((s > maps_s[prev_wp + 1]) && (prev_wp < (int)(maps_s.size() - 1)))
+    // Transform from Frenet s,d coordinates to Cartesian x,y
+    vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
     {
-        prev_wp++;
+        int prev_wp = -1;
+        
+        while((s > maps_s[prev_wp + 1]) && (prev_wp < (int)(maps_s.size() - 1)))
+        {
+            prev_wp++;
+        }
+        
+        int wp2 = (prev_wp + 1) % maps_x.size();
+        double heading = atan2((maps_y[wp2] - maps_y[prev_wp]), (maps_x[wp2] - maps_x[prev_wp]));
+        
+        // The x,y,s along the segment
+        double seg_s = (s - maps_s[prev_wp]);
+        double seg_x = maps_x[prev_wp] + seg_s*cos(heading);
+        double seg_y = maps_y[prev_wp] + seg_s*sin(heading);
+        
+        double perp_heading = heading - (pi() / 2);
+        double x = seg_x + (d * cos(perp_heading));
+        double y = seg_y + (d * sin(perp_heading));
+        
+        return {x,y};
     }
-    
-    int wp2 = (prev_wp + 1) % maps_x.size();
-    double heading = atan2((maps_y[wp2] - maps_y[prev_wp]), (maps_x[wp2] - maps_x[prev_wp]));
-    
-    // The x,y,s along the segment
-    double seg_s = (s - maps_s[prev_wp]);
-    double seg_x = maps_x[prev_wp] + seg_s*cos(heading);
-    double seg_y = maps_y[prev_wp] + seg_s*sin(heading);
-    
-    double perp_heading = heading - (pi() / 2);
-    double x = seg_x + (d * cos(perp_heading));
-    double y = seg_y + (d * sin(perp_heading));
-    
-    return {x,y};
 }
 
 int main()
@@ -165,9 +185,6 @@ int main()
     
     // Waypoint map to read from
     string map_file_ = "../data/highway_map.csv";
-    
-    // The max s value before wrapping around the track back to 0
-    double max_s = 6945.554;
     
     ifstream in_map_(map_file_.c_str(), ifstream::in);
     
@@ -260,8 +277,8 @@ int main()
                 
                 //cout << "____________________________" << endl;
                 //cout << "lane: " << lane << ", car_s: " << car_s << endl;
-                uint32_t nearest_car_ahead_on_left = car_s + 6945.554;
-                uint32_t nearest_car_ahead_on_right = car_s + 6945.554;
+                uint32_t nearest_car_ahead_on_left = car_s + kMaxS;
+                uint32_t nearest_car_ahead_on_right = car_s + kMaxS;
                 
                 for (int i = 0; i < sensor_fusion.size(); i++)
                 {
@@ -338,8 +355,6 @@ int main()
                 
                 // Behavior: Let's see what to do
                 double speed_diff = 0;
-                const double MAX_SPEED = 49.5;
-                const double MAX_ACC = .224;
                 if (is_car_ahead)
                 {
                     bool is_left_lane_free = !is_car_on_left && (lane > 0);
@@ -349,7 +364,7 @@ int main()
                         // Check which lane is better - less traffic
                         if (nearest_car_ahead_on_right > nearest_car_ahead_on_left)
                         {
-                            cout << "both lanes are free, right lane is better, " << "nearest_car_ahead_on_left: " << nearest_car_ahead_on_left << "nearest_car_ahead_on_right: " << nearest_car_ahead_on_right << endl;
+                            cout << "both lanes are free, right lane is better, " << "nearest_car_ahead_on_left: " << nearest_car_ahead_on_left << ", nearest_car_ahead_on_right: " << nearest_car_ahead_on_right << endl;
                             lane++;
                         }
                         else
@@ -369,14 +384,14 @@ int main()
                     }
                     else
                     {
-                        speed_diff -= MAX_ACC;
+                        speed_diff -= kMaxAcc;
                     }
                 }
                 else
                 {
-                    if (ref_vel < MAX_SPEED)
+                    if (ref_vel < kMaxSpeed)
                     {
-                        speed_diff += MAX_ACC;
+                        speed_diff += kMaxAcc;
                     }
                 }
                 
@@ -463,13 +478,13 @@ int main()
                 for(int i = 1; i < 50 - prev_size; i++)
                 {
                     ref_vel += speed_diff;
-                    if (ref_vel > MAX_SPEED)
+                    if (ref_vel > kMaxSpeed)
                     {
-                        ref_vel = MAX_SPEED;
+                        ref_vel = kMaxSpeed;
                     }
-                    else if (ref_vel < MAX_ACC)
+                    else if (ref_vel < kMaxAcc)
                     {
-                        ref_vel = MAX_ACC;
+                        ref_vel = kMaxAcc;
                     }
                     
                     double N = target_dist / (0.02 * ref_vel / 2.24);
